b72d75021681eff69bfc81857b2d3905c997b630
Priya Sharma <sharmapriya18504@gmail.com>
2026-05-03 09:00:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -144,7 +144,10 @@ elif section == "📄 Power BI + PDF":
         with open(file, "rb") as f:
             base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
-
+        pdf_display = f"""
+        <iframe src="data:application/pdf;base64,{base64_pdf}" 
+        width="100%" height="700"></iframe>
+        """
 
         st.markdown(pdf_display, unsafe_allow_html=True)
 
